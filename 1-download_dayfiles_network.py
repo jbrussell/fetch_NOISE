3827e3b4e91a34778b04777acc934e27c36b56dd
Josh Russell <jbrussell93@gmail.com>
2025-04-14 09:22:32 -0400

    Fix bug checking for PA

--- 1-download_dayfiles_network.py
+++ 1-download_dayfiles_network.py
@@ -134,7 +134,7 @@ for inet in range(0,len(inventory)):
                 if is_removeresp:
                     try:
                         # Check whether pressure channel, if so use "VEL" option which doesn't add or remove zeros
-                        if st[0].stats.response.instrument_sensitivity.input_units == 'PA':
+                        if st[0].stats.response.instrument_sensitivity.input_units.upper() == 'PA':
                             st.remove_response(output='VEL', zero_mean=True, taper=True, taper_fraction=0.05, pre_filt=[0.001, 0.005, sr/3, sr/2], water_level=600)
                         else:
                             st.remove_response(output=outunits, zero_mean=True, taper=True, taper_fraction=0.05, pre_filt=[0.001, 0.005, sr/3, sr/2], water_level=600)
